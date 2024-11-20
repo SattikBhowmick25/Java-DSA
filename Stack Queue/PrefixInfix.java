4b9fc22cd47761952a188bbe85f042c0e9e5f7df
Sattik Bhowmick <sattikbhowmick2002@gmail.com>
2024-11-20 08:23:10 +0530

    PrefixInifix.java added

---
+++ Stack Queue/PrefixInfix.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class PrefixInfix{
+    public static String prefixInfix(String prefix){
+        Stack<String> st=new Stack<>();
+
+        for(int l=prefix.length()-1;l>=0;l--){
+            char c=prefix.charAt(l);
+            if( (c>='A' && c<='Z') && (c>='a' && c<='z') && (c>='0' && c<='9') ){
+                st.push(Character.toString(c));
+            }
+            else{
+                String op1=st.peek();   st.pop();
+                String op2=st.peek();   st.pop();
+                String s='(' + op1 + c + op2 + ')';
+                st.push(s);
+            }
+        }
+        return st.peek();
+    }
+}
