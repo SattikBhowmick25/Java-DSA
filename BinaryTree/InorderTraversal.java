06a0a61bb79bdaf3e9df55602d0c4deaba207a16
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-04-24 01:25:21 +0530

    Iterative inorder traversal added

--- BinaryTree/InorderTraversal.java
+++ BinaryTree/InorderTraversal.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class TreeNode {
     int val;
@@ -25,5 +26,26 @@ public class InorderTraversal {
         inorder(root.left,inList);
         inList.add(root.val);
         inorder(root.right,inList);
+    }
+    //Iterative inorder
+    public List<Integer> iterativeInorderTraversal(TreeNode root){
+        List<Integer> inorder=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                inorder.add(node.val);
+                node=node.right;
+            }
+        }
+        return inorder;
     }
 }
