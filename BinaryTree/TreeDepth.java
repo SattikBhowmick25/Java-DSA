5b0d4c0bc1cc4bac78269cb8d13827484a36d160
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-04-26 14:47:31 +0530

    MaxDepth added

---
+++ BinaryTree/TreeDepth.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class TreeDepth {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int depth=0;
+        Queue<TreeNode> q=new LinkedList<>();
+
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode front=q.poll();
+                if(front.left!=null){
+                    q.add(front.left);
+                }
+                if(front.right!=null){
+                    q.add(front.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+    
+}
