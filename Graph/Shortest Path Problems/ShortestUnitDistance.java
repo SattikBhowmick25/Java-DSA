c12dbe6a1538bc1a038f59f4d386c69706fce47f
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-07-27 17:28:33 +0530

    ShortestPathUG added

---
+++ Graph/Shortest Path Problems/ShortestUnitDistance.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class ShortestUnitDistance{
+    public int[] shortestPath(int[][] edges, int N, int M) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<M;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]); 
+        }
+
+        int[] dist=new int[N];
+        for(int i=0;i<N;i++){
+            dist[i]=(int)1e9;
+        }
+        dist[0]=0;
+        Queue<Integer> q=new LinkedList<>();
+        q.add(0);
+        
+        while(!q.isEmpty()){
+            int node=q.poll();
+            q.remove();
+
+            for(int it:adj.get(node)){
+                if(dist[it]>dist[node]+1){
+                    dist[it]=dist[node]+1;
+                    q.add(it);
+                }
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            if(dist[i]==(int)1e9)
+                dist[i]=-1;
+        }
+        return dist;
+    }
+}
