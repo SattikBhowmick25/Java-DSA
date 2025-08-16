9f188548a9551e67a888d799f286d49c709f3f74
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-08-16 14:00:28 +0530

    BinaryMaze

--- Graph/Shortest Path Problems/BinaryMaze1.java
+++ Graph/Shortest Path Problems/BinaryMaze1.java
@@ -54,5 +54,53 @@ public class BinaryMaze1 {
                 }
             }
         }
+        return -1;
+    }
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1; 
+        if (n == 1 && m == 1 && grid[0][0] == 0) return 1;
+
+        Queue<Tuple> q = new LinkedList<>();
+        int[][] dist = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = (int) 1e9;
+            }
+        }
+
+        dist[0][0] = 1;  
+        q.add(new Tuple(1, 0, 0));
+
+        int[] nr = {-1,-1,0,1,1,1,0,-1};
+        int[] nc = {0,1,1,1,0,-1,-1,-1};
+
+        while (!q.isEmpty()) {
+            Tuple temp = q.poll();
+            int dis = temp.first;
+            int r = temp.second;
+            int c = temp.third;
+
+            for (int i = 0; i < 8; i++) {
+                int nearR = r + nr[i];
+                int nearC = c + nc[i];
+
+                if (nearR >= 0 && nearR < n && nearC >= 0 && nearC < m 
+                    && grid[nearR][nearC] == 0 && dis + 1 < dist[nearR][nearC]) {
+
+                    dist[nearR][nearC] = dis + 1;
+
+                    if (nearR == n - 1 && nearC == m - 1) return dis + 1;
+
+                    q.add(new Tuple(dis + 1, nearR, nearC));
+                }
+            }
+        }
+
+        return -1;
     }
 }
