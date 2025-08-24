a329a934500f05e5ea9d3808e021eb03638213de
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-08-24 18:32:47 +0530

    BFS-DFS: 01 Matrix

---
+++ Graph/BFS-DFS/01Matrix.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+public class 01Matrix {
+    public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> q=new LinkedList<>();
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] dist=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    dist[i][j]=0;
+                    q.add(new int[]{i,j});
+                }
+                else{
+                    dist[i][j]=Integer.MAX_VALUE;
+                }
+            }
+        }
+        int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
+
+        while(!q.isEmpty()){
+            int cur[]=q.poll();
+            int row=cur[0];
+            int col=cur[1];
+
+            for(int[] d:dir){
+                int nr=row+d[0];
+                int nc=col+d[1];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n){
+                    if(dist[nr][nc]>dist[row][col]+1){
+                        dist[nr][nc]=dist[row][col]+1;
+                        q.add(new int[]{nr,nc});
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+}
