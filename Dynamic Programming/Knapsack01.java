8b62460ae37bca7687ef06fc8656e26d7a7971e6
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-08-27 12:48:20 +0530

    DP: 01 knapsack tabulation

--- Dynamic Programming/Knapsack01.java
+++ Dynamic Programming/Knapsack01.java
@@ -35,4 +35,44 @@ public class Knapsack01 {
 
         return t[n][w];
     }
+
+    // Top down approach
+    static int knapsackTopDown(int W, int val[], int wt[]) {
+        int n = val.length;
+        int[][] dp = new int[n + 1][W + 1]; // dp[i][j] → max value using first i items and capacity j
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0; // base case: 0 items or 0 capacity
+                } else if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]],
+                            dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
+    static int knapsackOptimized(int W, int val[], int wt[]) {
+        int n = val.length;
+        int[] dp = new int[W + 1]; // Step 1: Initialize
+
+        // Step 2: Process each item
+        for (int i = 0; i < n; i++) {
+
+            // Step 3: Traverse capacity backwards
+            for (int j = W; j >= wt[i]; j--) {
+
+                // Step 4: Update DP value (take or skip item)
+                dp[j] = Math.max(dp[j], val[i] + dp[j - wt[i]]);
+            }
+        }
+
+        // Step 5: Final result
+        return dp[W];
+    }
+
 }
